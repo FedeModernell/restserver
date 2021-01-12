520a31db35f7f5ae4af3ac5b39bd5ccab7e4725e
Fede <modernellfederico@gmail.com>
2021-01-11 21:33:23 -0300

    Variable entorno m

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urldb;
 if (process.env.NODE_ENV == 'dev') {
     urldb = 'mongodb://localhost:27017/cafe';
 } else {
-    urldb = 'mongodb+srv://fedemodernell:Irunp2oPuNzHQc0d@cluster0.s58wr.mongodb.net/cafe';
+    urldb = process.env.MONGO_URI;
 };
 
 
